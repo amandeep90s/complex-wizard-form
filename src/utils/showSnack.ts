8bc3907eda164fe49012b8687d0c92c7a387a09e
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-05 18:21:47 +0530

    feat: Add showSnack utility function for displaying notifications

---
+++ src/utils/showSnack.ts
@@ -0,0 +1,17 @@
+import { enqueueSnackbar, OptionsObject, VariantType } from 'notistack';
+
+type ShowSnackOptions = Partial<OptionsObject> & {
+  variant?: VariantType;
+  duration?: number;
+};
+
+const showSnack = (message: string, options: ShowSnackOptions = {}) => {
+  const defaultOptions: ShowSnackOptions = {
+    variant: 'success',
+    duration: 3000
+  };
+
+  return enqueueSnackbar(message, { ...defaultOptions, ...options });
+};
+
+export { showSnack };
