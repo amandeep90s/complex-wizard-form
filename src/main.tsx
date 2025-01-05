dbe5191c1c9d8d7c5e4b609e34a644e13d35db14
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-05 18:08:33 +0530

    feat: Integrate MUI ThemeProvider and create custom theme configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import { RoutesWrapper } from '@/routes';
+import { theme } from '@/utils/theme';
+import { ThemeProvider } from '@mui/material';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RoutesWrapper />
+    <ThemeProvider theme={theme}>
+      <RoutesWrapper />
+    </ThemeProvider>
   </StrictMode>
 );
