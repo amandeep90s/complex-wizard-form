5e2646817f01f670eb76333697d9dfa66dbbe5fa
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-05 18:31:09 +0530

    feat: Add SnackbarProvider for enhanced notification handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { RoutesWrapper } from '@/routes';
 import { theme } from '@/utils/theme';
 import { ThemeProvider } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <SnackbarProvider />
       <RoutesWrapper />
     </ThemeProvider>
   </StrictMode>
