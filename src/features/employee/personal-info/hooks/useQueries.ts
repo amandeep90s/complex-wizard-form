c931c15415fe847523e84324043fe752b91ab4d2
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-06 20:08:37 +0530

    feat: Implement hooks for managing states and cities in employee personal info form

---
+++ src/features/employee/personal-info/hooks/useQueries.ts
@@ -0,0 +1,25 @@
+import { getCities, getStates } from '@/features/employee/personal-info/utils/api';
+
+import { Schema } from '@/features/employee/personal-info/types/schema';
+import { useFormContext } from '@/features/form/hooks/useFormContext';
+import { useQuery } from '@tanstack/react-query';
+import { useWatch } from 'react-hook-form';
+
+const useStates = () => {
+  return useQuery({
+    queryKey: ['states'],
+    queryFn: getStates
+  });
+};
+
+const useCities = () => {
+  const { control } = useFormContext<Schema>();
+  const state = useWatch({ control, name: 'state' });
+
+  return useQuery({
+    queryKey: ['cities', { state }],
+    queryFn: () => getCities(state)
+  });
+};
+
+export { useCities, useStates };
