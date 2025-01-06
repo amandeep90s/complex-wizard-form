bbbada5a8a385bab9e4899065250e8d21db9dfcd
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-06 09:14:18 +0530

    feat: Add ErrorMessage and FormErrorSummary components for improved form error handling

---
+++ src/features/form/error-message.tsx
@@ -0,0 +1,22 @@
+import { ErrorMessage as RHFErrorMessage } from '@hookform/error-message';
+import { FormHelperText } from '@mui/material';
+import { ArrayPath, FieldValues, Path } from 'react-hook-form';
+
+type ErrorMessageProps<T extends FieldValues> = { name: Path<T> | ArrayPath<T> };
+
+const ErrorMessage = <T extends FieldValues>({ name }: ErrorMessageProps<T>) => {
+  return (
+    <>
+      <RHFErrorMessage
+        name={name}
+        as={<FormHelperText error />}
+      />
+      <RHFErrorMessage
+        name={`${name}.root`}
+        as={<FormHelperText error />}
+      />
+    </>
+  );
+};
+
+export { ErrorMessage };
