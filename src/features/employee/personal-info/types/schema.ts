dac6eed72e5ff42d1db1c227f084aa9d226aa798
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-06 18:42:34 +0530

    feat: Add employee personal info schema with validation for fields and default values

---
+++ src/features/employee/personal-info/types/schema.ts
@@ -0,0 +1,35 @@
+import { calculatePastDate } from '@/utils/calculatePastDate';
+import { regex } from '@/utils/regex';
+import validator from 'validator';
+import { z } from 'zod';
+
+const schema = z.object({
+  firstName: z.string().min(1).max(50),
+  lastName: z.string().min(1).max(50),
+  email: z.string().email(),
+  phoneNumber: z
+    .string()
+    .min(1)
+    .refine((value) => validator.isMobilePhone(value, 'en-US')),
+  dateOfBirth: z.coerce.date().max(calculatePastDate(18)).min(calculatePastDate(100)),
+  state: z.string().min(1),
+  city: z.string().min(1),
+  streetAddress: z.string().min(1),
+  socialSecurityNumber: z.union([z.string().regex(regex.socialSecurityNumber), z.literal('')])
+});
+
+type Schema = z.infer<typeof schema>;
+
+const defaultValues: Schema = {
+  city: '',
+  dateOfBirth: calculatePastDate(18),
+  email: '',
+  firstName: '',
+  lastName: '',
+  phoneNumber: '',
+  state: '',
+  streetAddress: '',
+  socialSecurityNumber: ''
+};
+
+export { defaultValues, schema as employeePersonalInfoSchema, schema, type Schema };
