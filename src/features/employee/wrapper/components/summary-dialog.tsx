5eb57772f83809cc8d0b70526a367c55b8ae0500
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-07 13:48:02 +0530

    feat: Add employee review functionality with terms and conditions handling and state management

---
+++ src/features/employee/wrapper/components/summary-dialog.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SummaryDialog = () => {
+  return <div>summary-dialog</div>;
+};
--- src/features/employee/wrapper/page.tsx
+++ src/features/employee/wrapper/page.tsx
@@ -1,7 +1,17 @@
+import { Divider } from '@mui/material';
 import { Outlet } from 'react-router';
+import { Stepper } from '@/features/employee/wrapper/components/stepper';
+import { SummaryDialog } from '@/features/employee/wrapper/components/summary-dialog';
 
-export const Page = () => {
-  return <Outlet />;
+const Page = () => {
+  return (
+    <>
+      <SummaryDialog />
+      <Stepper />
+      <Divider sx={{ marginY: 2 }} />
+      <Outlet />
+    </>
+  );
 };
 
 export { Page as EmployeeWrapper };
