2315073a2ebc4dc1b27306f67bc9a13cbc176664
Amandeep Singh <programmeramandeep@gmail.com>
2025-01-07 13:48:53 +0530

    feat: Add route for EmployeeReview component in the application

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 
-import { DashboardLayout } from './features/layout/components/dashboard-layout';
-import { EmployeeAdditionalInfo } from './features/employee/additional-info/page';
-import { EmployeeHistory } from './features/employee/history/page';
-import { EmployeePersonalInfo } from './features/employee/personal-info/page';
-import { EmployeeSkills } from './features/employee/skills/page';
-import { EmployeeWrapper } from './features/employee/wrapper/page';
+import { DashboardLayout } from '@/features/layout/components/dashboard-layout';
+import { EmployeeAdditionalInfo } from '@/features/employee/additional-info/page';
+import { EmployeeHistory } from '@/features/employee/history/page';
+import { EmployeePersonalInfo } from '@/features/employee/personal-info/page';
+import { EmployeeReview } from '@/features/employee/review/page';
+import { EmployeeSkills } from '@/features/employee/skills/page';
+import { EmployeeWrapper } from '@/features/employee/wrapper/page';
 
 const RoutesWrapper = () => {
   return (
@@ -34,7 +35,10 @@ const RoutesWrapper = () => {
             path="/employee/additional-info"
             element={<EmployeeAdditionalInfo />}
           />
-          <Route path="/employee/review" />
+          <Route
+            path="/employee/review"
+            element={<EmployeeReview />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
